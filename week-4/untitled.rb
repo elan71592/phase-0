7f44e7e5aec23b0bc927491e079aa786124b9369
Elan Kvitko <elan@idknyc.com>
2016-03-03 11:58:47 -0500

    done exercise 1

--- week-4/untitled.rb
+++ week-4/untitled.rb
@@ -1,27 +1,41 @@
-def hours(years)
-  hours_per_year = 0
-  if years == 1
-    puts "Would you like to know how many hours there are in a year or a leap year? Please respond with either \'year\' or \'leap year\' without the quotes."
+class Time
+  attr_accessor :hours_per_year
 
-    answer = gets.strip.downcase
+  def initialize
+    @hours_per_year = 0
+    @hours_per_decade = 87600
+  end
 
-    if answer == "leap year"
-      hours_per_year = 24 * 366
-      puts hours_per_year * years
-    elsif answer == "year"
-      hours_per_year = 24 * 365
-      puts hours_per_year
+  def hours(years)
+    if years == 1
+      puts "Would you like to know how many hours there are in a year or a leap year? Please respond with either \'year\' or \'leap year\' without the quotes."
+
+      answer = gets.strip.downcase
+
+      if answer == "leap year"
+        @hours_per_year = 24 * 366
+        puts "There are " + @hours_per_year.to_s + " hours in #{years} leap year!"
+      elsif answer == "year"
+        @hours_per_year = 24 * 365
+        puts "There are " + @hours_per_year.to_s + " hours in #{years} year!"
+      else
+        puts "Sorry, you did not answer with a valid response!"
+      end
     else
-      puts "Sorry, you did not answer with a valid response!"
+      @hours_per_year = 24 * 365
+      answer = @hours_per_year * years
+      puts "There are " + answer.to_s + " hours in #{years} years!"
     end
-  else
-    hours_per_year = 24 * 365
-    puts hours_per_year * years
+  end
+
+  def mins(decades)
+    minutes = @hours_per_decade * 60
+    result = minutes * decades
+    puts "There are #{result} minutes in #{decades} decade/s!"
   end
 end
 
-hours(3)
-hours(1)
-
-
-
+Time.new.hours(3)
+Time.new.hours(15)
+Time.new.hours(10)
+Time.new.mins(8)
