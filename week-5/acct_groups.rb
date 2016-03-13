da88e7dd374dc1fc7758c2b24e1cfeabf54370e1
Elan Kvitko <elan@idknyc.com>
2016-03-12 23:47:41 -0500

    double refactor to the max yo

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -63,7 +63,14 @@ def acct_groups(names)
     end
     groups.pop
   end
-  groups.each {|group| puts "Group #{idx}: #{group}"; idx += 1 }
+  groups.each do |group|
+    puts
+    puts "Group #{idx}: "
+    group.each do |person|
+      puts person
+    end
+    idx += 1
+  end
 end
 
 names = ["Aarthi Gurusami", "Abid Ramay", "Adam Zmudzinski", "Alec Hendrickson", "Alex Wen", "Alicia Briceland", "Allison paul", "Andrey Slonski", "Anna Lansfjord", "Ben Sanecki", "Ben Flores", "Buck Melton", "Caitlin Hoffman", "Carlos Gonzalez", "Chand Nirankari", "Ché Sanders", "Chris Henderson", "Chris Lamkin", "Christyn Budzyna", "Dan Park", "David Ramirez", "David Tao", "David Walden", "Bill Deng", "Denny Jovic", "Daniel Deutsch", "Dexter Moran", "Diana Ozemebhoya Eromosele", "Dominick Lombardo", "Elan Kvitko","Elizabeth Alexander", "Elizabeth Brown", "Ena Bekanovic", "Esther Leytush", "Evan Druce", "Frank Lam", "Gabo", "Jack Thatcher", "Jason Milfred", "John Colella", "Jonathan Kaplan", "Kelson Adams", "Kristal Lam", "Kunal Patel", "Leland Meiners", "Liam Binell", "Lisa Buch", "Lisa Dannewitz", "Lyudmila Arinich", "Mohamed Monekata", "Parker Smathers", "Patrick DeWitte", "Renan Martins", "Riley", "Robin Soubry", "James Robinson", "Samantha Holmes", "Scott Southard", "Shaun R Sweet", "Shin Wang", "Sibel Ergener", "Simon Thomas", "Talal Talhouk", "Ted Bogin", "Traci Fong", "Victoria Solorzano"]
