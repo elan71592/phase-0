6a1c5068960de5db1f0fe8215e51c892b638267d
Elan Kvitko <elan@idknyc.com>
2016-03-16 23:31:20 -0400

    done gps

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -6,32 +6,29 @@
 
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, num_of_ingredients)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+def serving_size_calc(food, serving)
+  foods = {
+    "cookie" => 1,
+    "cake" =>  5,
+    "pie" => 7
+  }
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
+  unless foods.has_key?(food)
+    raise ArgumentError.new("#{food} is not a valid input")
   end
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  serving_size = foods[food]
+  people_left = serving % serving_size
 
-  serving_size = library.values_at(item_to_make)[0]
-  remaining_ingredients = num_of_ingredients % serving_size
-
-  case remaining_ingredients
+  case people_left
   when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+    "Calculations complete: Make #{serving / serving_size} of #{food}"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    "Calculations complete: Make #{(serving / serving_size) + 1} of #{food}, you have #{serving_size - people_left} leftover servings."
   end
 end
 
-p serving_size_calc("pie", 7)
+p serving_size_calc("pie", 72)
 p serving_size_calc("pie", 8)
 p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
@@ -39,4 +36,22 @@ p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
+
+
 #  Reflection
+
+# What did you learn about making code readable by working on this challenge?
+
+  # - I learned that reading others code can be difficult and stressful but at the same time it taught me that it is extremely important about writing my own code to be easily read by others.
+
+# Did you learn any new methods? What did you learn about them?
+
+  # - I haven't learned any new methods here however it was nice to see a case statement in practice.
+
+# What did you learn about accessing data in hashes?
+
+  # - I didn't learn anything extra apart from the other challenges in DBC that taught me how to access data in hashes.
+
+# What concepts were solidified when working through this challenge?
+
+  # - WRITE EASY TO READ CODE haha!
