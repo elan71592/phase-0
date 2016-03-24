17fe73bce980a23fff4c5be8a6b4448d2790792b
Elan Kvitko <elan@idknyc.com>
2016-03-24 14:36:15 -0400

    done pair challenge 1

---
+++ week-7/game.js
@@ -0,0 +1,36 @@
+ // Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description:
+// Overall mission:
+// Goals:
+// Characters:
+// Objects:
+// Functions:
+
+// Pseudocode
+
+
+// Initial Code
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
